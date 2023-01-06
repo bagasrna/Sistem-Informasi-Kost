d9a2a9d05e2e90e59f61e6ed365442ea9fdddac2
Natabagas <meganatabagas@gmail.com>
2023-01-06 12:49:08 +0700

    Dashboard done

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistem Informasi Kos</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-white overflow-hidden font-Poppins">
-    <aside class="fixed left-0 top-0 z-10 h-screen border-r border-white w-12 md:w-20 hover:w-48  md:hover:w-64 bg-gray-700 hover:shadow-2xl">
+<body class="bg-white overflow-x-hidden font-Poppins">
+    <aside class="fixed overflow-hidden left-0 top-0 z-10 h-screen w-12 md:w-20 hover:w-48  md:hover:w-[265px] bg-gray-700 hover:shadow-2xl">
         <div class="flex flex-col justify-between">
             <div>
                 <div class="h-16 flex items-center bg-gray-500">
-                    <a href="/dashboard" class="w-max block px-2 md:px-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class="md:w-10 w-8 h-8  text-white md:h-10" ><path fill-rule="evenodd" d="M11.622 1.602a.75.75 0 01.756 0l2.25 1.313a.75.75 0 01-.756 1.295L12 3.118 10.128 4.21a.75.75 0 11-.756-1.295l2.25-1.313zM5.898 5.81a.75.75 0 01-.27 1.025l-1.14.665 1.14.665a.75.75 0 11-.756 1.295L3.75 8.806v.944a.75.75 0 01-1.5 0V7.5a.75.75 0 01.372-.648l2.25-1.312a.75.75 0 011.026.27zm12.204 0a.75.75 0 011.026-.27l2.25 1.312a.75.75 0 01.372.648v2.25a.75.75 0 01-1.5 0v-.944l-1.122.654a.75.75 0 11-.756-1.295l1.14-.665-1.14-.665a.75.75 0 01-.27-1.025zm-9 5.25a.75.75 0 011.026-.27L12 11.882l1.872-1.092a.75.75 0 11.756 1.295l-1.878 1.096V15a.75.75 0 01-1.5 0v-1.82l-1.878-1.095a.75.75 0 01-.27-1.025zM3 13.5a.75.75 0 01.75.75v1.82l1.878 1.095a.75.75 0 11-.756 1.295l-2.25-1.312a.75.75 0 01-.372-.648v-2.25A.75.75 0 013 13.5zm18 0a.75.75 0 01.75.75v2.25a.75.75 0 01-.372.648l-2.25 1.312a.75.75 0 11-.756-1.295l1.878-1.096V14.25a.75.75 0 01.75-.75zm-9 5.25a.75.75 0 01.75.75v.944l1.122-.654a.75.75 0 11.756 1.295l-2.25 1.313a.75.75 0 01-.756 0l-2.25-1.313a.75.75 0 11.756-1.295l1.122.654V19.5a.75.75 0 01.75-.75z" clip-rule="evenodd" /></svg>
+                    <a href="/dashboard">
+                        <div class="w-max flex gap-4 items-center px-2 md:px-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class="md:w-10 w-8 h-8  text-white md:h-10" ><path fill-rule="evenodd" d="M11.622 1.602a.75.75 0 01.756 0l2.25 1.313a.75.75 0 01-.756 1.295L12 3.118 10.128 4.21a.75.75 0 11-.756-1.295l2.25-1.313zM5.898 5.81a.75.75 0 01-.27 1.025l-1.14.665 1.14.665a.75.75 0 11-.756 1.295L3.75 8.806v.944a.75.75 0 01-1.5 0V7.5a.75.75 0 01.372-.648l2.25-1.312a.75.75 0 011.026.27zm12.204 0a.75.75 0 011.026-.27l2.25 1.312a.75.75 0 01.372.648v2.25a.75.75 0 01-1.5 0v-.944l-1.122.654a.75.75 0 11-.756-1.295l1.14-.665-1.14-.665a.75.75 0 01-.27-1.025zm-9 5.25a.75.75 0 011.026-.27L12 11.882l1.872-1.092a.75.75 0 11.756 1.295l-1.878 1.096V15a.75.75 0 01-1.5 0v-1.82l-1.878-1.095a.75.75 0 01-.27-1.025zM3 13.5a.75.75 0 01.75.75v1.82l1.878 1.095a.75.75 0 11-.756 1.295l-2.25-1.312a.75.75 0 01-.372-.648v-2.25A.75.75 0 013 13.5zm18 0a.75.75 0 01.75.75v2.25a.75.75 0 01-.372.648l-2.25 1.312a.75.75 0 11-.756-1.295l1.878-1.096V14.25a.75.75 0 01.75-.75zm-9 5.25a.75.75 0 01.75.75v.944l1.122-.654a.75.75 0 11.756 1.295l-2.25 1.313a.75.75 0 01-.756 0l-2.25-1.313a.75.75 0 11.756-1.295l1.122.654V19.5a.75.75 0 01.75-.75z" clip-rule="evenodd" /></svg>
+                            <span class="text-white ml-[5px] text-[12px] md:text-[16px]">Sistem Informasi Kos</span>
+                        </div>
                     </a>
                 </div>
                 <div class="mt-6">
@@ -21,7 +24,7 @@
                         <li class="w-max space-y-4 -ml-[10px] font-medium hover:w-full">
                             <a href="" class="block md:w-[52px] w-[30px] ml-[5px] md:ml-0 rounded-full py-1 md:py-3 bg-sky-500">
                                 <div class="w-max flex gap-4 items-center px-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class='md:w-7 w-5 h-5 md:h-7  text-white -ml-[11px] md:-ml-[4px]' ><path d="M6 3a3 3 0 00-3 3v2.25a3 3 0 003 3h2.25a3 3 0 003-3V6a3 3 0 00-3-3H6zM15.75 3a3 3 0 00-3 3v2.25a3 3 0 003 3H18a3 3 0 003-3V6a3 3 0 00-3-3h-2.25zM6 12.75a3 3 0 00-3 3V18a3 3 0 003 3h2.25a3 3 0 003-3v-2.25a3 3 0 00-3-3H6zM17.625 13.5a.75.75 0 00-1.5 0v2.625H13.5a.75.75 0 000 1.5h2.625v2.625a.75.75 0 001.5 0v-2.625h2.625a.75.75 0 000-1.5h-2.625V13.5z" /></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class='md:w-7 w-5 h-5 md:h-7  text-white -ml-[11px] md:-ml-[4px]' ><path d="M6 3a3 3 0 00-3 3v2.25a3 3 0 003 3h2.25a3 3 0 003-3V6a3 3 0 00-3-3H6zM15.75 3a3 3 0 00-3 3v2.25a3 3 0 003 3H18a3 3 0 003-3V6a3 3 0 00-3-3h-2.25zM6 12.75a3 3 0 00-3 3V18a3 3 0 003 3h2.25a3 3 0 003-3v-2.25a3 3 0 00-3-3H6zM17.625 13.5a.75.75 0 00-1.5 0v2.625H13.5a.75.75 0 000 1.5h2.625v2.625a.75.75 0 001.5 0v-2.625h2.625a.75.75 0 000-1.5h-2.625V13.5z" /></svg>
                                     <span class='text-white ml-[10px]'>Dashboard</span>
                                 </div>
                             </a>
@@ -30,7 +33,7 @@
                         <li class="w-max">
                             <a href="">
                                 <div class="flex items-center gap-4 py-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class='md:w-6 w-4 h-4 md:h-6 text-white -pl-[30px]'><path d="M11.584 2.376a.75.75 0 01.832 0l9 6a.75.75 0 11-.832 1.248L12 3.901 3.416 9.624a.75.75 0 01-.832-1.248l9-6z" /><path fill-rule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 010 1.5H3a.75.75 0 010-1.5h.75v-9.918a.75.75 0 01.634-.74A49.109 49.109 0 0112 9c2.59 0 5.134.202 7.616.592a.75.75 0 01.634.74zm-7.5 2.418a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75zm3-.75a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0v-6.75a.75.75 0 01.75-.75zM9 12.75a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75z" clip-rule="evenodd" /><path d="M12 7.875a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" /></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  class='md:w-6 w-4 h-4 md:h-6 text-white -pl-[30px]'><path d="M11.584 2.376a.75.75 0 01.832 0l9 6a.75.75 0 11-.832 1.248L12 3.901 3.416 9.624a.75.75 0 01-.832-1.248l9-6z" /><path fill-rule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 010 1.5H3a.75.75 0 010-1.5h.75v-9.918a.75.75 0 01.634-.74A49.109 49.109 0 0112 9c2.59 0 5.134.202 7.616.592a.75.75 0 01.634.74zm-7.5 2.418a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75zm3-.75a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0v-6.75a.75.75 0 01.75-.75zM9 12.75a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75z" clip-rule="evenodd" /><path d="M12 7.875a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" /></svg>
                                     <span class='text-white ml-[15px]'>Data Penghuni</span>
                                 </div>
                             </a>
--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -5,11 +5,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Kos</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    @include('layout.navbar')
-    @include('layout.sidebar')
-    <div>
+    <div class="md:mx-[100px] mx-[80px]  flex flex-col z-40 font-Poppins">
+        <h1 class='font-bold text-[30px] mt-[30px] md:mt-[90px] '>Dashboard</h1>
+        <div class="flex flex-row justify-between flex-wrap mt-[20px]">
+            <div class="flex mb-[100px] text-white">
+                <a href="">
+                    <div class="md:w-[300px] mb-[50px] w-[250px] bg-blue-500 rounded-xl">
+                        <div class=" p-5">
+                            <p class="text-[24px] md:text-[30px]">6</p>
+                            <h1 class="font-bold text-[16px] md:text-[20px]">Jumlah Kamar</h1>
+                        </div>
+                        <div class="md:mt-[50px] mt-[20px] text-center rounded-b-xl p-2 bg-blue-800 w-full">
+                            <h1 class='text-bold'>Click For More Info!</h1>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="md:w-[80px] md:ml-[190px] text-gray-200 w-[50px] ml-[170px] -mt-[170px] md:-mt-[200px]"><path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" /><path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" /></svg>
+                </a>
+            </div>
+
+            <div class="flex  mb-[100px] text-white">
+                <a href="">
+                    <div class="md:w-[300px] mb-[50px] w-[250px] bg-yellow-500 rounded-xl">
+                        <div class=" p-5">
+                            <p class="text-[24px] md:text-[30px]">3</p>
+                            <h1 class="font-bold text-[16px] md:text-[20px]">Akun Penghuni</h1>
+                        </div>
+                        <div class="md:mt-[50px] mt-[20px] text-center rounded-b-xl p-2 bg-yellow-800 w-full">
+                            <h1 class='text-bold'>Click For More Info!</h1>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="md:w-[80px] md:ml-[190px] text-gray-200 w-[50px] ml-[170px] -mt-[170px] md:-mt-[200px]"><path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" /></svg>
+                </a>
+            </div>
+
+            <div class="flex  mb-[100px] text-white">
+                <a href="">
+                    <div class="md:w-[300px] mb-[50px] w-[250px] bg-red-500 rounded-xl">
+                        <div class=" p-5">
+                            <p class="text-[24px] md:text-[30px]">5</p>
+                            <h1 class="font-bold text-[16px] md:text-[20px]">Belum Bayar</h1>
+                        </div>
+                        <div class="md:mt-[50px] mt-[20px] text-center rounded-b-xl p-2 bg-red-800 w-full">
+                            <h1 class='text-bold'>Click For More Info!</h1>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="md:w-[80px] md:ml-[190px] text-gray-200 w-[50px] ml-[170px] -mt-[170px] md:-mt-[200px]"><path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm-4.34 7.964a.75.75 0 01-1.061-1.06 5.236 5.236 0 013.73-1.538 5.236 5.236 0 013.695 1.538.75.75 0 11-1.061 1.06 3.736 3.736 0 00-2.639-1.098 3.736 3.736 0 00-2.664 1.098z" clip-rule="evenodd" /></svg>
+                </a>
+            </div>
+
+            <div class="flex mb-[50px] text-white">
+                <a href="">
+                    <div class="md:w-[300px] mb-[50px] w-[250px] bg-green-500 rounded-xl">
+                        <div class=" p-5">
+                            <p class="text-[24px] md:text-[30px]">6</p>
+                            <h1 class="font-bold text-[16px] md:text-[20px]">Lunas</h1>
+                        </div>
+                        <div class="md:mt-[50px] mt-[20px] text-center rounded-b-xl p-2 bg-green-800 w-full">
+                            <h1 class='text-bold'>Click For More Info!</h1>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="md:w-[80px] md:ml-[190px] text-gray-200 w-[50px] ml-[170px] -mt-[170px] md:-mt-[200px]"><path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z" clip-rule="evenodd" /></svg>
+                </a>
+            </div>
+        </div>
     </div>
+    @include('layout.sidebar')
 </body>
 </html>
